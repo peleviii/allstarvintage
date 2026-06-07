9d9c60b24ae0f45e4c2145616bdd8f9a4bd23dcb
peleviii <peleviii@hotmail.com>
2026-06-07 15:06:14 +0300

    sunday 2st semiaa

--- resources/views/matches/index.blade.php
+++ resources/views/matches/index.blade.php
@@ -400,7 +400,7 @@
                         <span class="font-medium text-gray-800 text-sm">Όμιλος Φιλάθλων Γέρακα</span>
                     </div>
                     <div class="flex flex-col items-center justify-center min-w-[90px] gap-1">
-                        <span class="text-gray-400 text-xs">14:00</span>
+                        <span class="text-gray-400 text-xs">0-2</span>
                     </div>
                     <div class="flex-1 text-left">
                         <span class="font-medium text-gray-800 text-sm">Volley Maniacs</span>
@@ -431,7 +431,7 @@
                 </div>
                 <div class="flex items-center gap-3">
                     <div class="flex-1 text-right">
-                        <span class="font-medium text-gray-800 text-sm">Νικητής</span>
+                        <span class="font-medium text-gray-800 text-sm">Volley Maniacs</span>
                     </div>
                     <div class="flex flex-col items-center justify-center min-w-[90px] gap-1">
                         <span class="text-gray-400 text-xs">16:30</span>
